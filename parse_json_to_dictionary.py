4b3c369c529733d27049456d5968d310cb49cb2e
krystianpietryka <krystian.pietryka@ggsitc.com>
2022-03-11 12:34:27 +0100

    Parsing json to create a 5 letter possible guess list

---
+++ parse_json_to_dictionary.py
@@ -0,0 +1,29 @@
+#Script loads json file as dictionary and removes all words of length different than 5
+import json
+
+with open('words_dictionary.json') as json_file:
+    data = json.load(json_file)
+    #print("Type:", type(data))
+
+keys_to_delete = []
+
+keys = data.keys()
+# mark letters with len different than 5 to delete
+for key in keys:
+    if len(key) != 5:
+        keys_to_delete.append(key)
+
+for i in range(len(keys_to_delete)):
+    del data[keys_to_delete[i]]
+
+keys = data.keys()
+#print(len(keys))
+
+#Wipe the contents of 5_letter_words.txt
+open('5_letter_words.txt', 'w').close()
+
+#Append dictionary keys to the 5_letter_word file
+with open('5_letter_words.txt', 'a') as file:
+    for key in keys:
+        file.write(key)
+        file.write('\n')
