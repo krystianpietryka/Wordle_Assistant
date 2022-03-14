cd9f4640e392e29982251dcc607b34f68d0cc934
krystianpietryka <krystian.pietryka@ggsitc.com>
2022-03-14 10:57:46 +0100

    Marking excluded letters added

--- Wordle_Answers_For_Input.py
+++ Wordle_Answers_For_Input.py
@@ -2,17 +2,22 @@ import re
 
 #Scripts displays the possible 5 letter answers for given regex pattern
 
-def Display_Possible_Answers(pattern):
+def Display_Possible_Answers(pattern, excluded_letters):
+    excluded_letters = excluded_letters.split()
     possible_answers = []
     regex_string = pattern
     with open('5_letter_words.txt', 'r') as words:
         for line in words.readlines():
             search_result = re.search(regex_string, line)
+            possible_flag = 1
             if search_result:
-                possible_answers.append(line)
-    print('\n', 'Possible Answers: \n')
-    for answer in possible_answers:
-        print(answer)
+                for letter in line:
+                    if letter in excluded_letters:
+                        possible_flag = 0
+                        break
+                if possible_flag == 1:
+                    possible_answers.append(line)
+    return possible_answers
     
 
-#Display_Possible_Answers('ho...')
+#Display_Possible_Answers('.me.t', 'pocraud')
